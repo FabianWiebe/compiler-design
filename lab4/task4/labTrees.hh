beb17c88ca09b17d0bf314d225fe8a3ffda62301
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-02-26 15:32:29 +0000

    implemented task 4

--- lab4/task4/labTrees.hh
+++ lab4/task4/labTrees.hh
@@ -52,33 +52,53 @@ public:
 
         void dump()
         {
-                cout << "BBlock @ " << this << endl;
-                cout << name << endl;
+                cout << "BBlock @ " << this;
+                cout << " (" << name << ")" << endl;
                 for(auto i : instructions)
                         i.dump();
-                cout << "True:  " << tExit << endl;
-                cout << "False: " << fExit << endl;
+                cout << "True:    " << tExit;
+                if (tExit) {
+                  cout << " (" << tExit->name << ")";
+                }
+                cout << std::endl << "False:   " << fExit;
+                if (fExit) {
+                  cout << " (" << fExit->name << ")";
+                }
+                std::cout << std::endl;
         }
 };
 int BBlock::nCounter = 0;
 
-
 /******************** Expressions ********************/
+
+std::ostream& indent(std::ostream& stream, int depth) {
+  for (int i = 0; i < depth; ++i) {
+    stream << " ";
+  }
+  return stream;
+}
+
 class Expression
 {
+private:
+        static int tmp_counter;
 public:
-        string name;
+        const string name;
 
-        Expression() : name("")
+        Expression(const std::string& name) : name(name)
         {
         }
-        virtual string makeNames() 
+        virtual std::string makeNames() 
         {
           // Lecture 8 / slide 11.
           // Virtual (but not pure) to allow overriding in the leaves.
+          return "_t" + std::to_string(tmp_counter++);
         }
         virtual string convert(BBlock*) = 0; // Lecture 8 / slide 12.
+        
+        virtual void dump(std::ostream& stream=std::cout, int depth = 0) = 0;
 };
+int Expression::tmp_counter = 0;
 
 
 class Add : public Expression
@@ -87,15 +107,126 @@ public:
         Expression *lhs, *rhs;
 
         Add(Expression* lhs, Expression* rhs) :
-                lhs(lhs), rhs(rhs)
+                Expression("+"), lhs(lhs), rhs(rhs)
         {
         }
 
-        string convert(BBlock* out)
+        virtual string convert(BBlock* out)
         {
-                // Write three address instructions to output
+          auto gen_name = makeNames();
+          auto left = lhs->convert(out);
+          auto right = rhs->convert(out);
+          out->instructions.emplace_back(gen_name, '+', left, right);
+          return gen_name;
         }
 
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << name << std::endl;
+          lhs->dump(stream, depth+1);
+          rhs->dump(stream, depth+1);
+        }
+};
+
+class Mult : public Expression
+{
+public:
+        Expression *lhs, *rhs;
+
+        Mult(Expression* lhs, Expression* rhs) :
+                Expression("*"), lhs(lhs), rhs(rhs)
+        {
+        }
+
+        virtual string convert(BBlock* out)
+        {
+          auto gen_name = makeNames();
+          auto left = lhs->convert(out);
+          auto right = rhs->convert(out);
+          out->instructions.emplace_back(gen_name, '*', left, right);
+          return gen_name;
+        }
+
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << name << std::endl;
+          lhs->dump(stream, depth+1);
+          rhs->dump(stream, depth+1);
+        }
+};
+
+class Variable : public Expression
+{
+public:
+        std::string var_name;
+
+        Variable(std::string var_name) :
+                Expression("var"), var_name(var_name)
+        {
+        }
+
+        virtual std::string makeNames() {
+          return var_name;
+        }
+
+        virtual string convert(BBlock* out)
+        {
+          return makeNames();
+          
+        }
+
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << var_name << std::endl;
+        }
+};
+
+class Constant : public Expression
+{
+public:
+        int value;
+
+        Constant(int value) :
+                Expression("constant"), value(value)
+        {
+        }
+
+        virtual std::string makeNames()  {
+          return std::to_string(value);
+        }
+
+        virtual string convert(BBlock* out)
+        {
+          return makeNames();
+          
+        }
+
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << value << std::endl;
+        }
+};
+
+class Equality : public Expression
+{
+public:
+        Expression *lhs, *rhs;
+
+        Equality(Expression* lhs, Expression* rhs) :
+                Expression("=="), lhs(lhs), rhs(rhs)
+        {
+        }
+
+        virtual string convert(BBlock* out)
+        {
+          auto gen_name = makeNames();
+          auto left = lhs->convert(out);
+          auto right = rhs->convert(out);
+          out->instructions.emplace_back(gen_name, '=', left, right);
+          return gen_name;
+          
+        }
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << name << std::endl;
+          lhs->dump(stream, depth+1);
+          rhs->dump(stream, depth+1);
+        }
 };
 
 
@@ -103,12 +234,14 @@ public:
 class Statement
 {
 public:
-        string name;
+        const string name;
 
-        Statement()
+        Statement(const std::string& name) : name(name)
         {
         }
         virtual BBlock* convert(BBlock *) = 0;
+
+        virtual void dump(std::ostream& stream=std::cout, int depth = 0) = 0;
 };
 
 
@@ -119,13 +252,91 @@ public:
         Expression *rhs;
 
         Assignment(string lhs, Expression *rhs) :
-                lhs(new Variable(lhs)), rhs(rhs)
+                Statement("A"), lhs(new Variable(lhs)), rhs(rhs)
         {
         }
 
         BBlock* convert(BBlock *out)
         {
-                // Write three address instructions to output
+          auto left = lhs->convert(out);
+          auto right = rhs->convert(out);
+          out->instructions.emplace_back(left, 'c', right, right);
+          return out;
+        }
+
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << "Statement(" << name << ")" << std::endl;
+          lhs->dump(stream, depth+1);
+          rhs->dump(stream, depth+1);
+        }
+};
+
+class Seq : public Statement
+{
+public:
+        std::list<Statement*> statements;
+
+        Seq(initializer_list<Statement*> statements) :
+                Statement("S"), statements(statements)
+        {
+        }
+
+        BBlock* convert(BBlock *out)
+        {
+          BBlock *current_out = out;
+          for (Statement* statement : statements) {
+            current_out = statement->convert(current_out);
+          }
+          return current_out;
+        }
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << "Statement(" << name << ")" << std::endl;
+          for (Statement* statement : statements) {
+            statement->dump(stream, depth+1);
+          }
+        }
+};
+
+
+class If : public Statement
+{
+public:
+        Equality *condition;
+        Statement *true_branch, *false_branch;
+
+        If(Equality *condition, Statement *true_branch, Statement *false_branch) :
+                Statement("I"), condition(condition), true_branch(true_branch), false_branch(false_branch)
+        {
+        }
+
+        BBlock* convert(BBlock *out)
+        {
+          condition->convert(out);
+          BBlock *true_block = new BBlock();
+          BBlock *false_block = NULL;
+          out->tExit = true_block;
+          true_block = true_branch->convert(true_block);
+          if (false_branch) {
+            false_block = new BBlock();
+            out->fExit = false_block;
+            false_block = false_branch->convert(false_block);
+          }
+          BBlock *join_block = new BBlock();
+          true_block->tExit = join_block;
+          if (false_branch) {
+            false_block->tExit = join_block;
+          } else {
+            out->fExit = join_block;
+          }
+          return join_block;
+        }
+        void dump(std::ostream& stream=std::cout, int depth = 0) {
+          indent(stream, depth) << "Statement(" << name << ")" << std::endl;
+          condition->dump(stream, depth+1);
+          true_branch->dump(stream, depth+1);
+          if (false_branch) {
+            false_branch->dump(stream, depth+1);
+          }
         }
 };
 
@@ -160,6 +371,41 @@ Statement *test = new Seq({
                                   new Add(
                                           new Variable("x"),
                                           new Constant(1)
+                                  )
+                          )
+});
+
+Statement *test2 = new Seq({
+                          new Assignment(
+                                  "x",
+                                  new Add(
+                                          new Variable("x"),
+                                          new Constant(1)
+                                  )
+                          ),new Assignment(
+                                  "y",
+                                  new Add(
+                                          new Variable("y"),
+                                          new Constant(1)
+                                  )
+                          ),new If(
+                                  new Equality(
+                                          new Variable("x"),
+                                          new Constant(0)
+                                  ),new If(
+                                          new Equality(
+                                                  new Variable("y"),
+                                                  new Constant(0)
+                                          ),new Assignment(
+                                                  "x",
+                                                  new Constant(1)
+                                          ), new Assignment(
+                                                  "y",
+                                                  new Constant(2)
+                                          )
+                                  ), new Assignment(
+                                          "y",
+                                          new Constant(3)
                                   )
                           )
 });
--- lab4/task4/main.cc
+++ lab4/task4/main.cc
@@ -2,6 +2,16 @@
 
 int main(int argc, char **argv)
 {
-	dumpCFG(test);
+	// test->dump();
+	// std::cout << "convertIf" << std::endl;
+	// BBlock *first_block = new BBlock();
+	// test->convert(first_block);
+	// dumpCFG(first_block);
+	// std::cout << std::endl << std::endl << "Test Case 2" << std::endl;
+	test2->dump();
+	std::cout << "convertIfs" << std::endl;
+	BBlock *first_block = new BBlock();
+	test2->convert(first_block);
+	dumpCFG(first_block);
 	return 0;
 }
